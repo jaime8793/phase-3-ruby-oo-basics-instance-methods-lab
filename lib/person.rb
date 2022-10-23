a2c2604b475697c1c6bcd898821c9e7d315d1c2b
James Owiti Maurice <james.owiti@student.moringaschool.com>
2022-10-23 22:35:06 +0300

    Completed Assignment

---
+++ lib/person.rb
@@ -0,0 +1,13 @@
+class Person
+    def talk
+        puts "Hello World!"
+    end
+    def walk
+        puts "The Person is walking"
+    end
+end
+
+adele_goldberg = Person.new
+adele_goldberg.talk
+alan_kay = Person.new
+alan_kay.talk
---
+++ ruby.rb
@@ -0,0 +1,8 @@
+
+class Family
+end
+
+James = Family.new
+Pamela = Family.new
+Veronica = Family.new
+James.methods
